d13e8e979aaa59008c6c617459c0f5ea1ef1ec2f
nawid1 <72757097+nawid1@users.noreply.github.com>
2021-04-28 11:28:17 +0200

    LoginLavet

--- app/src/main/java/com/foodwaste/foodwastevaluetracker/RegisterActivity.java
+++ app/src/main/java/com/foodwaste/foodwastevaluetracker/RegisterActivity.java
@@ -64,7 +64,7 @@ public class RegisterActivity extends AppCompatActivity {
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
                     Toast.makeText(RegisterActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                    startActivity(new Intent(RegisterActivity.this, StartActivity.class));
                     finish();
                 }else{
                     Toast.makeText(RegisterActivity.this, "Registration Failed", Toast.LENGTH_SHORT).show();
