bbeb5863568d4f70f930b7520ef890553a75e6e6
nawid1 <72757097+nawid1@users.noreply.github.com>
2021-05-03 13:02:40 +0200

    Retrieve

---
+++ app/src/main/java/com/foodwaste/foodwastevaluetracker/FoodItem.java
@@ -0,0 +1,35 @@
+package com.foodwaste.foodwastevaluetracker;
+
+import java.util.Date;
+
+public class FoodItem {
+
+    String name;
+    int price;
+    int usage;
+    Date dateTyped;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getUsage() {
+        return usage;
+    }
+
+    public Date getDateTyped() {
+        return dateTyped;
+    }
+
+    public FoodItem(String name, int price, int usage, Date dateTyped){
+
+
+
+    }
+
+
+}
